3bebca6094fb54aced5b60e446da5ac40b92f35b
the-a7k <vojta.lyson@seznam.cz>
2024-01-17 14:01:52 +0100

    Basic disk reading

---
+++ disk.asm
@@ -0,0 +1,37 @@
+loaddisk:
+    pusha
+    push dx
+
+    mov ah, 2       ; Read initialization
+
+    mov al, dh      ; HDD sectors to read
+    mov cl, 2       ; HDD start sector number
+    mov ch, 0       ; HDD cylinder number
+    mov dh, 0       ; HDD head number
+
+    int 0x13        ; Disk access IRQ
+    jc disk_err
+
+    pop dx
+    cmp al, dh
+    jne sector_err
+    popa
+    ret
+
+    disk_err:
+        mov si, DISK_ERROR
+        call asciiout
+        mov dh, ah
+        call hexout
+        jmp disk_loop
+
+    sector_err:
+        mov si, SECTORS_ERROR
+        call asciiout
+
+    disk_loop:
+        jmp $
+
+
+DISK_ERROR: db "Disk read error", 0
+SECTORS_ERROR: db "Incorrect number of sectors read", 0
--- stdout.asm
+++ stdout.asm
@@ -1,21 +1,57 @@
 asciiout:
+    pusha
     str_loop:
-        mov al, [si]    ; Moves current char into 'al' register
-        cmp al, 0       ; Check if current char is 0
-        jne print_char  ; Jump if-not-equal
-        ret             ; Function finish
+        mov al, [si]
+        cmp al, 0               ; Check if current char is 0 (end of string)
+        jne print_char
+        popa
+        ret
 
     print_char:
-        mov ah, 0x0e    ; Print initializer
-        int 0x10        ; Print value inside of 'al' register
-        inc si          ; Next char in variable
-        jmp str_loop    ; Jump to next character
+        mov ah, 0x0e            ; TTY mode
+        int 0x10                ; Print IRQ
+        inc si                  ; Next char in variable
+        jmp str_loop
 
 
 newline:
     mov ah, 0x0e
-    mov al, 0x0a
+    mov al, 0x0a                ; ASCII line feed
     int 0x10
-    mov al, 0x0d
+    mov al, 0x0d                ; ASCII carriage return
     int 0x10
     ret
+
+
+hexout:
+    pusha
+    mov cx, 0                   ; Counter 0-4
+
+    hex_loop:
+        cmp cx, 4               ; Fixed length of 4
+        je print_hex
+
+        mov ax, di
+        and ax, 0x000f          ; Get last number
+        add ax, 0x30            ; Hex to ASCII offset
+        cmp ax, 0x39            ; Compare if larger then ASCII 0-9
+        jle save_hex
+        add ax, 0x07            ; Convert to A-F if NaN
+
+        save_hex:
+            mov si, HEX_STR + 5
+            sub si, cx
+            mov [si], al
+            ror di, 4           ; Rotate next number to the end
+            inc cx
+            jmp hex_loop
+
+        print_hex:
+            mov si, HEX_STR
+            call asciiout
+            popa
+            ret
+
+
+HEX_STR: 
+    db '0x0000', 0
